499efa6c9eeaf173dab96d959ae6545b05fd0b85
alexcologia33 <alexcologia33@gmail.com>
2019-05-12 01:43:07 -0300

    dev alterado server.js e git ignore para heroku

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const appName = 'espire'
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/'+appName));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/'+appName+'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
